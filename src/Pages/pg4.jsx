b0961bf9f71a22855da5963b320f083909ad88c8
Thawn <laanglamthawn01@gmail.com>
2025-02-15 15:35:17 +0530

    update grammar

--- src/Pages/pg4.jsx
+++ src/Pages/pg4.jsx
@@ -3,7 +3,7 @@ import './pg4.css'
 const pg4 = () => {
   return (
     <div className='bg'>
-        <p className="br">Thankyou for being my valentine</p>
+        <p className="br">Thank you for being my valentine</p>
         <p className="pr">I will never leave you.</p>
         <h1>💗</h1>
         <a href="/pg3">
